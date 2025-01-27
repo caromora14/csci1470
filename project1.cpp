1585f10a12429dde4cc2e49f156a8a568cc0a9a1
Carolina Morales <carolinamoralesrdz14@gmail.com>
2025-01-26 20:17:42 -0600

    Create project1.cpp

---
+++ project1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//Input
+	int small;
+	int large;
+	float total;
+	float tax;
+
+
+
+	//Processing
+	cout << "Number of small rooms: ";
+	cin >> small;
+	cout << "Number of large rooms: ";
+		cin >> large;
+
+		cout << "Price per small rooms: $25.00";
+		cout << "Price per large rooms; $35.00";
+
+		total = (25 * small) + (35 * large)
+			cout<< "Total: " << total;
+		cout<<tax
+
+
+
+	//Output
+
+	return 0;
+}
