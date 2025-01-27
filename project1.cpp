0c2b037851fef0d0921c3a44c5ae7fa8b00c5bc1
Carolina Morales <carolinamoralesrdz14@gmail.com>
2025-01-26 21:59:44 -0600

    Add files via upload

--- project1.cpp
+++ project1.cpp
@@ -1,3 +1,15 @@
+/////////////////////////////////////////////////////////////////////
+//
+// Teammates Names: Carolina morales
+// Date: 01/27/2025
+// Class: CSCI 1470.03
+// Semester: Spring 2025
+// Instructor: Dr. Jonatan Reyes
+//
+// Program Name: Lab1
+// Program Description: Find total cost of small and large rooms with tax
+//
+/////////////////////////////////////////////////////////////////////
 #include <iostream>
 using namespace std;
 
@@ -7,8 +19,7 @@ int main() {
 	int large;
 	float total;
 	float tax;
-
-
+	float total_tax;
 
 	//Processing
 	cout << "Number of small rooms: ";
@@ -16,16 +27,24 @@ int main() {
 	cout << "Number of large rooms: ";
 		cin >> large;
 
-		cout << "Price per small rooms: $25.00";
-		cout << "Price per large rooms; $35.00";
-
-		total = (25 * small) + (35 * large)
-			cout<< "Total: " << total;
-		cout<<tax
-
-
+		total = (25 * small) + (35 * large);
+		tax= 0.0825*total;
+		total_tax= total*tax;
 
 	//Output
+		cout<<"Estimate for Carpet Cleaning Service";
+		cout<<"-------------------------------------";
+		cout<<"Number of small rooms: "<<small;
+		cout<<"Number of large rooms: "<<large;
+		cout << "Price per small rooms: $25.00";
+		cout << "Price per large rooms: $35.00";
+		cout<<"------------------------------------";
+		cout << "Total: $" << total;
+		cout<<"Tax: $"<<tax;
+		cout<<"===================================";
+		cout<< "Total w/Tax: $"<<total_tax;
+
+		cout<< "This estimate is valid for 30 days.";
 
 	return 0;
 }
